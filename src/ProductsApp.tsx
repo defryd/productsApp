bfdd3a6cf8f975c1bd09efafd9c60206ea760172
‘Freddy <elipson39z@gmail.com>
2025-03-28 15:12:41 -0500

    StackNavigator configuration

--- src/ProductsApp.tsx
+++ src/ProductsApp.tsx
@@ -1,9 +1,10 @@
-import { Text, View } from 'react-native'
+import { NavigationContainer } from '@react-navigation/native'
+import { StackNavigator } from './presentation/navigation/StackNavigatior'
 
 export const ProductsApp = () => {
     return (
-        <View>
-            <Text>ProductsApp</Text>
-        </View>
+        <NavigationContainer>
+            <StackNavigator />
+        </NavigationContainer>
     )
 }
